c239e3c8bba3d20b8f9c4c88a25bcf6a3d585395
nahed2019 <nanawalid@gmail.com>
2021-03-16 16:55:04 +0000

    Added midia, fixtures file and products model

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Product, Category, Occasion
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'rating',
+        'product_image',
+    )
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 
 # Register your models here.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Occasion)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Occasion(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    occasion = models.ManyToManyField('Occasion')
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    product_image = models.ImageField(null=False, blank=True)
+
+    def __str__(self):
+        return '{}, {}, {}'.format(self.name, self.category, self.occasion)
