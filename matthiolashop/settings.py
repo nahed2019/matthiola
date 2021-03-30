8412e6f59a3844147f50af2998b341fb066a3810
nahed2019 <nanawalid@gmail.com>
2021-03-30 21:01:10 +0000

    config to push to heroku

--- matthiolashop/settings.py
+++ matthiolashop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'z@&7n_kb%lk9^_q-mbv61mn-6e(r-m(3a)4#&7e0l#cpy=zh*4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['matthiola.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,13 +120,17 @@ WSGI_APPLICATION = 'matthiolashop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
