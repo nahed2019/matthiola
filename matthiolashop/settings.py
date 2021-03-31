e248e6622c3663cdc50be428721fb75f128f96c7
nahed2019 <nanawalid@gmail.com>
2021-03-31 19:13:43 +0000

    recovery the Secret key in siitting file

--- matthiolashop/settings.py
+++ matthiolashop/settings.py
@@ -22,7 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "7rfbuu0(7t1d&m+ropk&6o_#zqoy90fh6gls$%j-+-2g*ifj@_"
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -51,6 +56,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
